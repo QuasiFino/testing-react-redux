233fe9021bcc8b4d88e62c3eac8005a134259264
wink-aajlak <aajlak.wink@gmail.com>
2021-12-16 14:16:13 +0530

    beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "../App";
+import { shallow } from 'enzyme'
+import App from "components/App";
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-
-  ReactDOM.render(<App />, div);
-
-  // console.log(div.innerHTML);
-
-  // not good approach
-  expect(div.innerHTML).toContain('Comment Box');
-
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it('shows comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
+
+// find returns array of instances of the specified element
