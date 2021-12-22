8407f8e0921be0de1860221b15026ea01037f4ae
wink-aajlak <aajlak.wink@gmail.com>
2021-12-22 10:33:08 +0530

    testing reducers and actions, creating commentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export default () => {
-  return (
-    <div>Comment List</div>
-  );
+class CommentList extends React.Component {
+  renderedComments() {
+    return this.props.comments.map( comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Comments</h1>
+        <ul>
+          {this.renderedComments()}
+        </ul>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state) => {
+  return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
